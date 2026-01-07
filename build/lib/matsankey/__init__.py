d8fda061d1d15392eaec1455a788fe6af56fc172
Xinya HAO <lisshall717@gmail.com>
2026-01-07 20:41:22 +0800

    Delete build directory

--- build/lib/matsankey/__init__.py
+++
@@ -1,6 +0,0 @@
-
-""" Make simple, pretty Sankey Diagrams """
-
-from matsankey.sankey import sankey, PySankeyException, NullsInFrame, LabelMismatch
-
-__all__ = ["sankey", "PySankeyException", "NullsInFrame", "LabelMismatch"]
